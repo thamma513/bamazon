76a99588e8c66f70c8279369be45928fd5575afa
thamma513 <thamma513@gmail.com>
2018-05-30 22:22:51 -0600

    bamazonCustomer working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,8 +8,13 @@ var connection = mysql.createConnection({
     user: "root",
 
     password: "root",
-    database: "bamazon_db;"
+    database: "bamazon_db"
 });
+
+// connection.connect(function() {
+//     if (err){throw (err)};
+//     start();
+// })
 
 function validateInput(value) {
     var integer = Number.isInteger(parseFloat(value));
@@ -27,46 +32,46 @@ function purchaseProd() {
 
     inquirer.prompt([{
             type: "input",
-            name: "item_id",
+            name: "id",
             message: "Enter the ID of the item you would like to purchase.",
             validate: validateInput,
             filter: Number
         },
         {
             type: "input",
-            name: "quantity",
+            name: "stock_quantity",
             message: "Quantity for purchase:",
             validate: validateInput,
             filter: Number
         }
     ]).then(function (input) {
 
-        var item = input.item_id;
-        var quantity = input.quantity;
+        var item = input.id;
+        var stock_quantity = input.stock_quantity;
 
         var queryStr = 'SELECT * FROM products WHERE ?';
 
         connection.query(queryStr, {
-            item_id: item
-        }, function (err, data) {
+            id: item
+        }, function (err, res) {
             if (err) throw err;
 
-            if (data.length === 0) {
+            if (res.length === 0) {
                 console.log("Error, Not a current product id. Enter a product id.");
                 start();
 
             } else {
-                var productData = data[0];
+                var products = res[0];
 
-                if (quantity <= product.quantity) {
+                if (stock_quantity <= products.stock_quantity) {
                     console.log("The requested product is in stock, placing your order...");
 
-                    var updateQueryStr = "UPDATE products SET quantity = " + (productData.quantity - quantity);
+                    var updateQueryStr = "UPDATE products SET stock_quantity = " + (products.stock_quantity - input.stock_quantity);
 
-                    connection.query(updateQueryStr, function (err, data) {
+                    connection.query(updateQueryStr, function (err, res) {
                         if (err) throw err;
 
-                        console.log('Your total is $' + productData.price * quantity);
+                        console.log('Your total is $' + products.price * stock_quantity);
                         console.log('Please come back to see us soon!');
                         console.log("\n-------------------------------------------------------------------\n");
 
@@ -90,28 +95,29 @@ function purchaseProd() {
 
 function start() {
 
-    queryStr = "SELECT * FROM products";
+    var queryStr = "SELECT id, product_name, department_name, stock_quantity, price FROM products";
     
-    connection.query(queryStr, function (err, data) {
+    connection.query(queryStr, function (err, res) {
         if (err) throw err;
 
         console.log('Current Product Inventory: ');
         console.log('.............................................\n');
 
         var prodList = '';
-        for (var i = 0; i < data.length; i++) {
+        for (var i = 0; i < res.length; i++) {
             prodList = '';
-            prodList += 'Item ID: ' + data[i].item_id + '  //  ';
-            prodList += 'Product Name: ' + data[i].product_name + '  //  ';
-            prodList += 'Department: ' + data[i].department_name + '  //  ';
-            prodList += 'Price: $' + data[i].price + '\n';
+            prodList += 'Item ID: ' + res[i].id + '  //  ';
+            prodList += 'Product Name: ' + res[i].product_name + '  //  ';
+            prodList += 'Department: ' + res[i].department_name + '  //  ';
+            prodList += 'Quantity: ' + res[i].stock_quantity + ' // ';
+            prodList += 'Price: $' + res[i].price + '\n';
 
             console.log(prodList);
         }
 
         console.log("---------------------------------------------------------------------\n");
 
-        //Prompt the user for item/quantity they would like to purchase
+        //Prompt the user for item/stock_quantity they would like to purchase
         purchaseProd();
     })
 }
